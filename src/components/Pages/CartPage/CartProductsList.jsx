5ebc9580a2dc54a6afc54ae0a250ec4bf909c309
Rajpal@123 <you@example.com>
2024-07-09 20:02:47 +0530

    cart item details

--- src/components/Pages/CartPage/CartProductsList.jsx
+++ src/components/Pages/CartPage/CartProductsList.jsx
@@ -1,6 +1,7 @@
-import { Container, Grid, IconButton } from '@mui/material'
+import { Container, Grid, IconButton, Typography } from '@mui/material'
 import React from 'react'
 import CloseIcon from '@mui/icons-material/Close';
+import SecondaryButton from '../../buttons/SecondaryButton';
 
 const CartProductsList = () => {
   return (
@@ -11,11 +12,23 @@ const CartProductsList = () => {
                       <CloseIcon />
                   </IconButton>
               </Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
-              <Grid item></Grid>
+              <Grid item>
+                  <img src="" alt="item-pic" />
+              </Grid>
+              <Grid item>
+                  <Typography variant='h6'>Title</Typography>
+              </Grid>
+              <Grid item>
+                  <Typography variant='h6'>Price</Typography>
+              </Grid>
+              <Grid item>
+                  <Typography variant='body2'>Availability</Typography>
+              </Grid>
+              <Grid item>
+                  <SecondaryButton>
+                      Add To Cart
+                  </SecondaryButton>
+              </Grid>
           </Grid>  
     </Container>
   )
