27696ea8f025a727967f849b7618d2b45f4b418e
Rajpal@123 <you@example.com>
2024-07-09 19:55:09 +0530

    grid add

--- src/components/Pages/CartPage/CartProductsList.jsx
+++ src/components/Pages/CartPage/CartProductsList.jsx
@@ -1,10 +1,11 @@
-import { Container } from '@mui/material'
+import { Container, Grid } from '@mui/material'
 import React from 'react'
 
 const CartProductsList = () => {
   return (
       <Container>
-          
+          <Grid container>
+          </Grid>  
     </Container>
   )
 }
